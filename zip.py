28992d26e32ba4d17c5aa1e44a164b5d4f377eeb
ross <rostik_novosiadlo@ukr.net>
2019-07-06 01:23:21 +0300

    implemented zip function

---
+++ zip.py
@@ -0,0 +1,17 @@
+a = [1, 2, 3]
+b = [7, 8, 9]
+
+
+def my_zip(lst1, lst2):
+    new_list = []
+    for x in range(0, len(lst1)):
+        new_list.append((lst1[x], lst2[x]))
+    return new_list
+
+
+def my_zip2(lst1, lst2):
+    return list(map(lambda x, y: (x, y), lst1, lst2))
+
+
+def my_zip3(lst1, lst2):
+    return list((lst1[x], lst2[x]) for x in range(0, len(lst1)))
